c09756786942f0b770f7a4211daedefb81837458
Stephanie de Brito Leal <sbrittoleal.dev@gmail.com>
2024-10-21 00:22:18 -0300

    Create.php feito e gerando novas entradas no banco

---
+++ create.php
@@ -0,0 +1,19 @@
+<?php
+include 'databaseconnection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $stmt = $pdo->prepare("INSERT INTO tasks (title, description) VALUES (:title, :description)");
+    $stmt->execute([
+        'title' => $title,
+        'description' => $description
+    ]);
+}
+?>
+
+<form method="POST">
+    <input type="text" name="title" placeholder="Título da Tarefa" required>
+    <input type="text" name="description" placeholder="Descrição da Tarefa">
+    <button type="submit">Adicionar Tarefa</button>
+</form>
